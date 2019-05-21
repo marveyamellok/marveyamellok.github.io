51d8446febb5700662561f88e127fbe642a2b05b
m.kuznecova <m.kuznecova@peppers-studio.ru>
2019-05-21 14:37:54 +0300

    - глаза почти как надо

--- components/eye/eye.js
+++ components/eye/eye.js
@@ -1,6 +1,6 @@
 $(function(){
-  $eye = $(".eye__item");
-  $apple = $(".eye__item-apple");
+  $eye = $(".eye__item"); //статичная часть, сам глаз целиком
+  $apple = $(".eye__item-apple"); //подвижная часть, радужка
   var x;
   var y;
   var appleX;
@@ -20,8 +20,8 @@ $(function(){
 
 
   function coords(event){
-    appleX = $($apple).position().top;
-    appleY = $($apple).position().left;
+    appleX = $($apple).position().left;
+    appleY = $($apple).position().top;
   }
 
   window.onmousemove = function(event){
@@ -35,28 +35,58 @@ $(function(){
       appleLeft = $($apple[i]).position().left;
       appleTop = $($apple[i]).position().top;
 
-      console.log(eyeLeft, eyeTop)
-      console.log(appleLeft, appleTop)
+      // console.log(eyeLeft, eyeTop)
+      // console.log(appleLeft, appleTop)
 
 
       appleNewX = x - eyeLeft; 
       appleNewY = y - eyeTop;
 
-      if ( x > eyeLeft + eyeWidth - appleWidth - appleLeft){
-        appleNewX = eyeWidth - appleWidth - appleLeft;
+      // if ( x > eyeLeft + eyeWidth - appleWidth - appleLeft){
+      //   appleNewX = eyeWidth - appleWidth - appleLeft;
+      // }
+
+      // if ( x < eyeLeft ){
+      //   appleNewX = appleLeft;
+      // }
+
+      // if ( y > eyeTop + eyeWidth - appleWidth - appleTop){
+      //   appleNewY = eyeWidth - appleWidth - appleTop;
+      // }
+
+      // if ( y < eyeTop ){
+      //   appleNewY = appleTop;
+      // }
+
+
+      // if ( x > eyeLeft && x < eyeLeft + eyeWidth){
+      //   // appleNewX = eyeWidth - appleWidth - appleLeft;
+      //   console.log("inside");
+      // }
+
+
+      if ( x < eyeLeft){
+        appleNewX = 5;
       }
 
-      if ( x < eyeLeft ){
-        appleNewX = appleLeft;
+
+      if ( x > eyeLeft + eyeWidth){
+        appleNewX = eyeWidth - appleWidth - 5;
       }
 
-      if ( y > eyeTop + eyeWidth - appleWidth - appleTop){
-        appleNewY = eyeWidth - appleWidth - appleTop;
+
+      if ( y < eyeTop){
+        appleNewY = 5;
       }
 
-      if ( y < eyeTop ){
-        appleNewY = appleTop;
+
+      if ( y > eyeTop + eyeWidth){
+        appleNewY = eyeWidth - appleWidth - 5;
       }
+
+      // if ( x < eyeLeft ){
+      //   appleNewX = appleLeft;
+      // }
       
       $($apple[i]).css({"left": appleNewX + "px", "top": appleNewY + "px"})
     }
